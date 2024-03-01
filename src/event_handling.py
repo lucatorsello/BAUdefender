d4c102765f37416ade9a652897cc3f5780188a78
lucatorsello <luca.torsello47@thehutgroup.com>
2024-03-01 15:11:08 +0000

    init

--- src/event_handling.py
+++ src/event_handling.py
@@ -1,8 +1,27 @@
 import pygame
 
 
-def handle_events():
+def handle_events(x_move, y_move):
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            return False
-    return True
+            return False, 0, 0
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                x_move += 1
+            if event.key == pygame.K_a:
+                x_move += -1
+            if event.key == pygame.K_s:
+                y_move += 1
+            if event.key == pygame.K_w:
+                y_move += -1
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_d:
+                x_move += -1
+            if event.key == pygame.K_a:
+                x_move += +1
+            if event.key == pygame.K_s:
+                y_move += -1
+            if event.key == pygame.K_w:
+                y_move += +1
+    return True, x_move, y_move
--- src/game_loop.py
+++ src/game_loop.py
@@ -3,28 +3,37 @@ import pygame
 from pygame.time import Clock
 from pygame.surface import Surface
 
+
 from event_handling import handle_events
 
 
 def game_loop(window: Surface, clock: Clock, FPS: int):
     current_dir = os.path.dirname(__file__)
-    grass = pygame.image.load(f"{current_dir}/graphics/grass.png").convert()
+    grass = pygame.image.load(f"{current_dir}/graphics/grass3.png").convert()
     game_runnning = True
-    c = 0
+    x_distance = 0
+    y_distance = 0
+    x_move = 0
+    y_move = 0
+    speed = 0.5
     while game_runnning:
-        c += 0.5
-        scroll_x = c % grass.get_width()
-        scroll_y = 0
-        x = -scroll_x
-        while x < window.get_width():
-            y = -scroll_y
-            while y < window.get_height():
-                window.blit(grass, (x, y))
+        game_runnning, x_move, y_move = handle_events(x_move=x_move, y_move=y_move)
+
+        x_distance += speed * x_move
+        y_distance += speed * y_move
+        scroll_x = x_distance % grass.get_width()
+        scroll_y = y_distance % grass.get_width()
+        x = 0
+
+        # as scroll increases blit on new x and y range
+        while x < window.get_width() + 100:
+            y = 0
+            while y < window.get_height() + 100:
+                window.blit(grass, (x - scroll_x, y - scroll_y))
                 y += grass.get_height()
             x += grass.get_width()
 
         clock.tick(FPS)
-        game_runnning = handle_events()
         pygame.display.update()
         pygame.display.flip()
 
